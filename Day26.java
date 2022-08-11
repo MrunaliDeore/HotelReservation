2c7c1fc0eb8cd6022af289c049d839d010ba8aad
MrunaliDeore <mrunalideoreohk1@gmail.com>
2022-08-11 21:54:13 +0530

    Added : simple welcome msg added into master branch

---
+++ Day26.java
@@ -0,0 +1,12 @@
+package WorkShop;
+
+public class Day26 {
+	
+	public static void main(String[] args){
+		
+		System.out.println("Day 26-Assignments\n");
+		System.out.println("Welcom to "
+				+ "Hotel Reservation Program");
+	}
+
+}
